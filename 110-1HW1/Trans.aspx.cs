a38db005a0202ee9f1a13274ae4df9867d999cc9
姍佑 施 <108111126@mail.aeust.edu.tw>
2021-10-13 18:58:29 +0800

    108111126

---
+++ 110-1HW1/Trans.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double d_value = -1;
+            d_value = 3.982;
+            double d_c = -1;
+            d_c = (d_value = 3.982 * 3.982);
+            Response.Write("d_value=" + d_value.ToString() + "F=" + d_c.ToString() + "C");
+        }
+    }
+}
